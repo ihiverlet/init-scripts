8a6326e7d5ec1103d3732f25427bbb83ca205a65
ihiverlet <94982379+ihiverlet@users.noreply.github.com>
2023-10-12 10:25:52 +0200

    Update rstudio.sh

--- rstudio.sh
+++ rstudio.sh
@@ -3,9 +3,12 @@
 echo \
 "
 setHook('rstudio.sessionInit', function(newSession) {
+    message('hello')
     devtools::install_github("rstudio/rstudioapi")
     message('Activation du dark mode')
     rstudioapi::applyTheme("Dracula")
 }, action = 'append')
 
 " >> /home/onyxia/.Rprofile
+
+chown root:users /home/onyxia/.Rprofile
