8bb6eb8bf285b0dbac523b4530bbcbff44ef60cb
ihiverlet <94982379+ihiverlet@users.noreply.github.com>
2023-10-12 09:14:15 +0200

    wip

--- rstudio.sh
+++ rstudio.sh
@@ -1,9 +1,20 @@
 #!/bin/sh
+WORK_DIR=/home/onyxia/work
+
 echo \
 "
 setHook('rstudio.sessionInit', function(newSession) {
     devtools::install_github("rstudio/rstudioapi")
     message('Activation du dark mode')
     rstudioapi::applyTheme("Dracula")
+}, action = 'append')
+
+setHook('rstudio.sessionInit', function(newSession) {
+  if (newSession && identical(getwd(), '${WORK_DIR}'))
+  {
+    devtools::install_github("rstudio/rstudioapi")
+    message('Activation du dark mode')
+    rstudioapi::applyTheme("Dracula")
+  }
 }, action = 'append')
 " >> /home/onyxia/.Rprofile
