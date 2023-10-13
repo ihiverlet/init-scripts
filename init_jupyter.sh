4a1a4e5b7ad463c0c16310e56d77a8a7df8297ba
ihiverlet <94982379+ihiverlet@users.noreply.github.com>
2023-10-13 17:34:24 +0200

    Update init_jupyter.sh

--- init_jupyter.sh
+++ init_jupyter.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo 'toto'
+touch /opt/mamba/share/jupyter/lab/settings/overrides.json
 echo "{'@jupyterlab/apputils-extension:themes': {'theme': 'JupyterLab Dark'}}" >> /opt/mamba/share/jupyter/lab/settings/overrides.json
