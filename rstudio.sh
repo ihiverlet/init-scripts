cab75009ce1675cca9ad2188d6f1a8d448d97ca4
ihiverlet <94982379+ihiverlet@users.noreply.github.com>
2023-10-16 11:03:24 +0200

    Update rstudio.sh

--- rstudio.sh
+++ rstudio.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-if command -v R; then
-    touch ${R_HOME}/etc/Rprofile.site
-    echo "if (Sys.getenv('DARK_MODE')=='TRUE'){
-        setHook('rstudio.sessionInit', function(newSession) {
-            rstudioapi::applyTheme("Dracula")
-        }, action = 'append')
-    }" >> ${R_HOME}/etc/Rprofile.site
+touch ${R_HOME}/etc/Rprofile.site
+echo "if (Sys.getenv('DARK_MODE')=='TRUE'){
+    setHook('rstudio.sessionInit', function(newSession) {
+        rstudioapi::applyTheme("Dracula")
+    }, action = 'append')
+}" >> ${R_HOME}/etc/Rprofile.site
