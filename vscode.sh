d6e0216b34030c327d506b381c7ce728f175770d
ihiverlet <94982379+ihiverlet@users.noreply.github.com>
2023-10-18 09:07:53 +0200

    Update vscode.sh

--- vscode.sh
+++ vscode.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-echo 'toto'
-export DARK_MODE='TRUE'
-jq '.|= . + {"workbench.colorTheme": "Default Dark Modern", }' ${HOME}/.local/share/code-server/User/settings.json  
+
+jq '. + {"workbench.colorTheme": "Default Dark Modern" }' ${HOME}/.local/share/code-server/User/settings.json > ${HOME}/tmp.settings.json  && mv ${HOME}/tmp.settings.json ${HOME}/.local/share/code-server/User/settings.json
