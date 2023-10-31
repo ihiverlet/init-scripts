5c31bc6e6ec9748e49def08e29e9a87754d193de
ihiverlet <94982379+ihiverlet@users.noreply.github.com>
2023-10-31 11:02:48 +0100

    Update duckdb.sh

--- duckdb.sh
+++ duckdb.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-if [[ -n $S3_ENDPOINT ]] && command -v duckdb ; then 
+if [[ -n $AWS_S3_ENDPOINT ]] && command -v duckdb ; then 
     echo -e "-- Duck head prompt
 .prompt '🦆 '
 -- Set s3 context
 CALL load_aws_credentials();
-SET s3_endpoint='$S3_ENDPOINT';" > ${HOME}/.duckdbrc 
+SET s3_endpoint='$AWS_S3_ENDPOINT';" > ${HOME}/.duckdbrc 
 fi
