ead1252512f1538c71f8d9a580bc68c47cb0c335
ihiverlet <94982379+ihiverlet@users.noreply.github.com>
2023-12-08 17:04:10 +0100

    Update zotero.sh

--- zotero.sh
+++ zotero.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
+mkdir -p ${HOME}/.config/rstudio
+touch ${HOME}/.config/rstudio/rstudio-prefs.json
 jq '. + {"zotero_libraries": "Web"}' ${HOME}/.config/rstudio/rstudio-prefs.json > ${HOME}/tmp.rstudio-prefs.json  && mv ${HOME}/tmp.rstudio-prefs.json ${HOME}/.config/rstudio/rstudio-prefs.json
 echo "toto";
