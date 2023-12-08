a1ad01e1124292ab261c0bdc3a27aca85bd12622
ihiverlet <94982379+ihiverlet@users.noreply.github.com>
2023-12-08 17:00:15 +0100

    Update zotero.sh

--- zotero.sh
+++ zotero.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 jq '. + {"zotero_libraries": "Web"}' ${HOME}/.config/rstudio/rstudio-prefs.json > ${HOME}/tmp.rstudio-prefs.json  && mv ${HOME}/tmp.rstudio-prefs.json ${HOME}/.config/rstudio/rstudio-prefs.json
+echo "toto";
