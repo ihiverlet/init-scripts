020597bae2e61ffe88b6dc29180be6c870866063
ihiverlet <94982379+ihiverlet@users.noreply.github.com>
2023-12-11 11:31:38 +0100

    Create init-helm-charts.sh

---
+++ init-helm-charts.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git clone https://github.com/InseeFrLab/helm-charts-interactive-services.git
+git clone https://github.com/InseeFrLab/helm-charts-databases.git
+git clone https://github.com/InseeFrLab/helm-charts-datavisualization.git
+git clone https://github.com/InseeFrLab/helm-charts-automation.git
+git clone https://github.com/InseeFrLab/helm-charts-shiny-apps.git
+git clone https://github.com/InseeFrLab/helm-charts-miscellaneous.git
+git clone https://inseefrlab.github.io/helm-charts-datascience
