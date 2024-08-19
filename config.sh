c3acdbb1f635b2f5ff6fb95c94095c9c090c66e7
ihiverlet <94982379+ihiverlet@users.noreply.github.com>
2024-08-20 00:04:20 +0200

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# kube aliases
+alias k='kubectl'
+alias kubetcl='kubectl'
+alias ka='kubectl apply '
+alias kg='kubectl get'
+alias kgp='kubectl get pods'
+alias kd='kubectl describe'
+alias kdp='kubectl describe pod'
+alias kdel='kubectl delete'
+alias kl='kubectl logs'
+alias kex='kubectl exec -it'
+alias kpf='kubectl port-forward'
+
+# helm aliases
+alias hl='helm ls'
+alias hi='helm install'
+alias huni='helm uninstall'
+alias hu='helm upgrade'
+
+# git aliases
+alias gc='git clone'
+alias gd='git diff'
+alias gcm='git commit -m'
+alias gs='git status'
+alias gl='git logs'
+alias glo='git logs --oneline'
+alias gp='git push'
+alias ga='git add'
+
+# divers
+alias stp='sudo !!'
+
+# config vim
+sudo tee -a /etc/vim/vimrc << EOT
+filetype plugin indent on 
+set showmatch
+set incsearch
+set mouse=a
+set number
+syntax on
+set incsearch
+EOT
