af675647e60b8d9be3b82845c153fe2f56f01f75
ihiverlet <94982379+ihiverlet@users.noreply.github.com>
2024-10-15 15:57:49 +0200

    Update rstudio-custom.sh

--- rstudio-custom.sh
+++ rstudio-custom.sh
@@ -13,19 +13,19 @@ setHook('rstudio.sessionInit', function(newSession) {
     # Console on the right
     rstudioapi::executeCommand('layoutConsoleOnRight')
     options(scipen=10)
-    options(editor="vim")
+    options(editor='vim')
     # define a default mirror 
-    options(repos = c(CRAN = "https://cran.rstudio.org"))
+    options(repos = c(CRAN = \"https:\/\/cran.rstudio.org\"))
     #Modify prompt cli and modify the number of decimals default
-    options(prompt="R> ", digits=4)
-    options(continue= "+++ ")
+    options(prompt=\"R> \", digits=4)
+    options(continue= \"+++ \")
     if (interactive()) {
       options(width = 120)
     }
     
     # Function to execute at R session startup
     .First <- function(){
-      cat(" Start ")
+      cat('Start')
     }
   }
 }, action = 'append')
