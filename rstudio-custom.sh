ded86f8ee4e0005517d2b078c17ed938396a2534
ihiverlet <94982379+ihiverlet@users.noreply.github.com>
2024-10-15 16:33:58 +0200

    Update rstudio-custom.sh

--- rstudio-custom.sh
+++ rstudio-custom.sh
@@ -4,15 +4,12 @@ echo \
 "
 
 setHook('rstudio.sessionInit', function(newSession) {
-  if (newSession && identical(getwd(), '/home/onyxia/work'))
-  {
     message('Activation du projet RStudio')
     rstudioapi::openProject('')
     # For a dark theme 
     rstudioapi::applyTheme('Dracula')
     # Console on the right
     rstudioapi::executeCommand('layoutConsoleOnRight')
-  }
 }, action = 'append')
 
 " >> /home/onyxia/.Rprofile
