a5132b3164bc955cc7decd3531e5cf8ce6efaeea
ihiverlet <94982379+ihiverlet@users.noreply.github.com>
2024-10-15 15:53:46 +0200

    Update rstudio-custom.sh

--- rstudio-custom.sh
+++ rstudio-custom.sh
@@ -2,33 +2,32 @@
 
 echo \
 "
+
 setHook('rstudio.sessionInit', function(newSession) {
-  if (newSession && identical(getwd(), '${WORKSPACE_DIR}'))
+  if (newSession && identical(getwd(), '/home/onyxia/work'))
   {
     message('Activation du projet RStudio')
-    rstudioapi::openProject('${PROJECT_DIR}')
+    rstudioapi::openProject('')
     # For a dark theme 
     rstudioapi::applyTheme('Dracula')
     # Console on the right
     rstudioapi::executeCommand('layoutConsoleOnRight')
-
-
     options(scipen=10)
-    options(editor=\"vim\")
+    options(editor="vim")
     # define a default mirror 
-    options(repos = c(CRAN = \"https://cran.rstudio.org\"))
+    options(repos = c(CRAN = "https://cran.rstudio.org"))
     #Modify prompt cli and modify the number of decimals default
-    options(prompt=\"R> \", digits=4)
-    options(continue= \"+++ \")
+    options(prompt="R> ", digits=4)
+    options(continue= "+++ ")
     if (interactive()) {
-    options(width = 120)
-      }
-
+      options(width = 120)
+    }
+    
     # Function to execute at R session startup
     .First <- function(){
-    cat(\" Start \")
+      cat(" Start ")
     }
+  }
+}, action = 'append')
 
-
-}
 " >> /home/onyxia/.Rprofile
