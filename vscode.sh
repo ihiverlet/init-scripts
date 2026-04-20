1a0c5974aab00dfc3da0a9d438e55986fc4eff84
ihiverlet <94982379+ihiverlet@users.noreply.github.com>
2026-04-20 12:08:19 +0200

    Disable welcome page onboarding in settings

--- vscode.sh
+++ vscode.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-jq '. + {"workbench.tips.enabled": false }' ${HOME}/.local/share/code-server/User/settings.json > ${HOME}/tmp.settings.json  && mv ${HOME}/tmp.settings.json ${HOME}/.local/share/code-server/User/settings.json
+jq '. + {"workbench.welcomePage.experimentalOnboarding": false }' ${HOME}/.local/share/code-server/User/settings.json > ${HOME}/tmp.settings.json  && mv ${HOME}/tmp.settings.json ${HOME}/.local/share/code-server/User/settings.json
