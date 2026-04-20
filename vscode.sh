f0022abbec8ec6cd9c72672d666413959ddef432
ihiverlet <94982379+ihiverlet@users.noreply.github.com>
2026-04-20 11:10:02 +0200

    Update VS Code settings to change startup editor

--- vscode.sh
+++ vscode.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-jq '. + {"workbench.welcomePage.experimentalOnboarding": "false" }' ${HOME}/.local/share/code-server/User/settings.json > ${HOME}/tmp.settings.json  && mv ${HOME}/tmp.settings.json ${HOME}/.local/share/code-server/User/settings.json
+jq '. + {"workbench.startupEditor": "terminal" }' ${HOME}/.local/share/code-server/User/settings.json > ${HOME}/tmp.settings.json  && mv ${HOME}/tmp.settings.json ${HOME}/.local/share/code-server/User/settings.json
