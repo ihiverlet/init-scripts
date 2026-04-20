c55f522174be0dcf7c981f00b56992b18e261cc3
ihiverlet <94982379+ihiverlet@users.noreply.github.com>
2026-04-20 11:22:29 +0200

    Enable workbench tips in vscode settings

--- vscode.sh
+++ vscode.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-jq '. + {"workbench.startupEditor": "terminal" }' ${HOME}/.local/share/code-server/User/settings.json > ${HOME}/tmp.settings.json  && mv ${HOME}/tmp.settings.json ${HOME}/.local/share/code-server/User/settings.json
+jq '. + {"workbench.tips.enabled": true }' ${HOME}/.local/share/code-server/User/settings.json > ${HOME}/tmp.settings.json  && mv ${HOME}/tmp.settings.json ${HOME}/.local/share/code-server/User/settings.json
